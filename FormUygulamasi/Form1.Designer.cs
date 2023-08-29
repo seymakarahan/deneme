dbc0d5401185d9175b245f09244dc3fae0ddb336
Durul Çamlı <dcamli@butel.com.tr>
2023-08-29 15:08:14 +0300

    DENEME

--- FormUygulamasi/Form1.Designer.cs
+++ FormUygulamasi/Form1.Designer.cs
@@ -45,34 +45,38 @@ namespace FormUygulamasi
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(333, 104);
+            this.checkBox1.Location = new System.Drawing.Point(444, 128);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.Size = new System.Drawing.Size(84, 20);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "Toplama";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 139);
+            this.textBox1.Location = new System.Drawing.Point(49, 171);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(171, 139);
+            this.textBox2.Location = new System.Drawing.Point(228, 171);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 108);
+            this.label1.Location = new System.Drawing.Point(45, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 3;
             this.label1.Text = "Sayı 1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -80,18 +84,20 @@ namespace FormUygulamasi
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(169, 108);
+            this.label2.Location = new System.Drawing.Point(225, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Sayı 2";
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(333, 128);
+            this.checkBox2.Location = new System.Drawing.Point(444, 158);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(64, 17);
+            this.checkBox2.Size = new System.Drawing.Size(79, 20);
             this.checkBox2.TabIndex = 5;
             this.checkBox2.Text = "Çıkarma";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -99,9 +105,10 @@ namespace FormUygulamasi
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(333, 151);
+            this.checkBox3.Location = new System.Drawing.Point(444, 186);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(62, 17);
+            this.checkBox3.Size = new System.Drawing.Size(77, 20);
             this.checkBox3.TabIndex = 6;
             this.checkBox3.Text = "Çarpma";
             this.checkBox3.UseVisualStyleBackColor = true;
@@ -109,9 +116,10 @@ namespace FormUygulamasi
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(333, 175);
+            this.checkBox4.Location = new System.Drawing.Point(444, 215);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(55, 17);
+            this.checkBox4.Size = new System.Drawing.Size(68, 20);
             this.checkBox4.TabIndex = 7;
             this.checkBox4.Text = "Bölme";
             this.checkBox4.UseVisualStyleBackColor = true;
@@ -119,36 +127,39 @@ namespace FormUygulamasi
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(596, 108);
+            this.label4.Location = new System.Drawing.Point(795, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.Size = new System.Drawing.Size(54, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Toplam";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(472, 136);
+            this.button1.Location = new System.Drawing.Point(629, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 12;
-            this.button1.Text = "Tamam";
+            this.button1.Text = "TAMAM";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(596, 141);
+            this.label3.Location = new System.Drawing.Point(795, 174);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.Size = new System.Drawing.Size(14, 16);
             this.label3.TabIndex = 13;
             this.label3.Text = "0";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -160,6 +171,7 @@ namespace FormUygulamasi
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
